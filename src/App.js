f8f142fdb2cd570fe32c08247c7841966146a985
Mugil99 <mugilya99@gmail.com>
2023-05-15 14:36:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+  let[lat,setLat]=useState();
+  const month=new Date().getMonth()+1;
+  let style={backgroundColor:""};
+  let iconSrc="";
+  let message="";
+   navigator.geolocation.getCurrentPosition((position)=>{
+    setLat(position.coords.latitude);
+   })
+   if(lat>0 && (month===12|| month<=2)){
+    message="Winter - Northern Hemisphere";
+    style={backgroundColor:"aqua"};
+    iconSrc="https://c.tadst.com/gfx/n/i/season-winter.svg";
+   }
+   else if(lat>0&& month>=3 && month<=5){
+    message = "Spring - Northern Hemisphere";
+    style = { backgroundColor: "limegreen" };
+    iconSrc = "https://c.tadst.com/gfx/n/i/season-spring.svg";
+   }
+   else if(lat>0 && month>=6 && month<=8){
+    message = "Summer - Northern Hemisphere";
+    style = { backgroundColor: "orangered" };
+    iconSrc = "https://c.tadst.com/gfx/n/i/season-summer.svg";
+   }
+   else if(lat>0 && month>=9 && month<=11){
+    message = "Autumn - Northern Hemisphere";
+    style = { backgroundColor: "yellow" };
+    iconSrc = "https://c.tadst.com/gfx/n/i/season-autumn.svg";
+  }
+  else {
+    message = "Error";
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+    <div id="icon">
+      <img id="icon_img" src={iconSrc} />
     </div>
-  );
+    <div id="lat">Lattitude - {lat}</div>
+    <div id="mes" style={style}> {message}</div>
+  </div>
+  )
 }
 
 export default App;
